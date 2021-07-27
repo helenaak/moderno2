6df25e513c8d83840c11a197f958f4e5c6db471b
helenaak <helenaak@mail.ru>
2021-07-27 20:38:51 +0300

    added page new-items

--- app/js/main.js
+++ app/js/main.js
@@ -2,9 +2,8 @@ $(function(){
     $(".rate-star").rateYo({
         rating: 5,
         starWidth: "12px",
-        readOnly: true
-       
-      });
+        readOnly: true 
+    });
     
 
     $(".products-slider__inner").slick({
@@ -12,9 +11,30 @@ $(function(){
         arrows: false,
         slidesToShow: 4,
         slidesToScroll: 4
-
-
     });
+
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: "$"
+        
+    });
+
+    $('.icon-th-list').on('click', function(){
+        $('.product__item').addClass('list');
+        $('.icon-th-list').addClass('active');
+        $('.icon-th-large').removeClass('active');
+    });
+    $('.icon-th-large').on('click', function(){
+        $('.product__item').removeClass('list');     
+        $('.icon-th-large').addClass('active');
+        $('.icon-th-list').removeClass('active');
+    });
+
+  
     var mixer = mixitup('.products__inner-box');
   
      
